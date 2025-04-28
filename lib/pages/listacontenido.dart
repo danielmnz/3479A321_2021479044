cfe5e54272715c8547b2a9571cebe4bc95d9fa81
daniel <102545398+danielmnz@users.noreply.github.com>
2025-04-27 20:39:59 -0400

    laboratorio5 cambios realizados

---
+++ lib/pages/listacontenido.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class About extends StatelessWidget {
+  const About({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('agregar 2 botones'),
+        ],
+      ),
+    ),);
+  }
+}
