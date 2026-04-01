c09568036e8b8775ea540ccd8a5f518253383881
ntdanii <102545398+danielmnz@users.noreply.github.com>
2026-04-01 12:21:36 -0300

    Avance3 MineCell widget

--- lib/main.dart
+++ lib/main.dart
@@ -68,12 +68,7 @@ class MinesweeperScreen extends StatelessWidget {
             ),
             itemCount: 64, //8x8 = 64 celdas
             itemBuilder: (context, index) {
-              return Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[400],
-                  border: Border.all(color: Colors.grey[600]!, width: 1.5),
-                ),
-              );
+              return MineCell(index: index); //clase widget del container
             },
           ),
         ),
@@ -82,4 +77,24 @@ class MinesweeperScreen extends StatelessWidget {
   }
 
   
+}
+
+class MineCell extends StatelessWidget {
+
+  final int index;
+
+  const  MineCell({ 
+    Key? key, 
+    required  this .index,  // Parámetro obligatorio  en el constructor 
+  }) :  super (key: key); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey[400],
+        border: Border.all(color: Colors.grey[600]!, width: 1.5),
+      ),
+    );
+  }
 }
