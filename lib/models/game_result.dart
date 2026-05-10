2a8f5c117ca1c36f7ce12957f13658536f6da908
ntdanii <102545398+danielmnz@users.noreply.github.com>
2026-05-10 17:02:41 -0400

    inclusión de lista y presentación del widget GameResult

--- lib/models/game_result.dart
+++ lib/models/game_result.dart
@@ -2,10 +2,12 @@ class GameResult {
  final String date; 
  final String timeSpent; 
  final bool isVictory; 
+ final String difficulty;
  
  GameResult({ 
    required this.date, 
    required this.timeSpent, 
-   required this.isVictory 
+   required this.isVictory,
+   required this.difficulty,
  }); 
 } 
--- lib/ui/screens/history_screen.dart
+++ lib/ui/screens/history_screen.dart
@@ -4,32 +4,50 @@ import 'package:probando_flutter_lab1/models/game_result.dart';
 class HistoryScreen extends StatelessWidget {
   HistoryScreen({super.key});
 
-  final GameResult game = GameResult(
-    date: '10 mayo',
-    timeSpent: '01:30',
-    isVictory: true,
-  );
+  final List<GameResult> listMaqueta = [
+    GameResult(
+      date: '10 mayo',
+      timeSpent: '01:30',
+      isVictory: true,
+      difficulty: 'Fácil',
+    ),
+    GameResult(
+      date: '11 mayo',
+      timeSpent: '02:40',
+      isVictory: false,
+      difficulty: 'Dificil',
+    ),
+    GameResult(
+      date: '12 mayo',
+      timeSpent: '03:50',
+      isVictory: true,
+      difficulty: 'Media',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Historial"),
-      ),
-      body: Center(
-        child: Card(
-          elevation: 4,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Fecha: ${game.date}'),
-                Text('Tiempo: ${game.timeSpent}')
-              ],
+      appBar: AppBar(title: Text("Historial")),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final game = listMaqueta[index];
+          return Card(
+            elevation: 4,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Fecha: ${game.date}'),
+                  Text('Tiempo: ${game.timeSpent}'),
+                  Text('Dificultad: ${game.difficulty}'),
+                ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
+        itemCount: listMaqueta.length,
       ),
     );
   }
